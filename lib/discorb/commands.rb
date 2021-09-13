d3381907266e207183486c5550b48b57abe1591c
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-13 22:01:33 +0900

    Initial commit

---
+++ lib/discorb/commands.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "commands/version"
+
+module Discorb
+  module Commands
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
